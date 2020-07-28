111f1d48562d6b1387b4f174b96ae1a2563a560b
Marcela Barella <marcela.barella@hotmail.com>
2020-07-27 22:32:51 -0300

    Add logs and fix health bars

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue({
         running: false,
         playerHealth: 100,
         monsterHealth: 100,
+        logs: []
     },
     computed: {
         hasResult() {
@@ -15,28 +16,36 @@ new Vue({
             this.running = true
             this.playerHealth = 100
             this.monsterHealth = 100
+            this.logs = []
         },
         attack(ult) {
-            this.damage('monsterHealth', 5, 10, ult)
-            this.damage('playerHealth', 7, 12, false)
+            this.damage('monsterHealth', 5, 10, ult, 'Player', 'Monster', 'player')
+            if(this.monsterHealth > 0) {
+                this.damage('playerHealth', 7, 12, false, 'Monster', 'Player', 'monster')
+            }
         },
-        damage(atr, min, max, ult){
+        damage(atr, min, max, ult, source, target, cls){
             const plus = ult ? 5 : 0
             const damage = this.getRandom(min + plus, max + plus)
             this[atr] = Math.max(this[atr] - damage, 0)
+            this.registerLog(`${source} hit ${target} with ${damage}`, cls)
         },
         heal(min, max) {
             const heal = this.getRandom(min, max)
             this.playerHealth = Math.min(this.playerHealth + heal, 100)
+            this.registerLog(`Player recovered ${heal}`, 'player')
         },
         healAndDamage() {
             this.heal(10, 15)
-            this.damage('playerHealth', 7, 12, false)
+            this.damage('playerHealth', 7, 12, false, 'Monster', 'Player', 'monster')
+        },
+        registerLog(text, cls) {
+            this.logs.unshift({ text, cls })
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
-        }
+        },
     },
     watch: {
         hasResult(value) {
